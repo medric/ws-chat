ab91ae692a7166b188ec8780e924d369bb6ff758
Chouan Médric <m.chouan@kreactive.com>
2016-10-19 16:02:50 +0200

    change webpack config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,49 +1,65 @@
-var path = require('path');
-var webpack = require('webpack');
-var AppCachePlugin = require('appcache-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+
+const paths = {
+    context: path.resolve(__dirname, '..', 'app'),
+    output:  path.resolve(__dirname, '..', 'dist'),
+}
+
+const d = function() {
+    console.log.apply(console, arguments);
+}
 
 // http://stackoverflow.com/questions/33502987/webpack-bundle-js-not-found
 // Expose
 module.exports = {
-  entry: [
-    'webpack-hot-middleware/client',
-    path.resolve(__dirname, '../app/index.js')
-  ],
-  output: {
-    path: path.resolve(__dirname, '../dist/'),
-    publicPath: '',
-    filename: 'bundle.js',
-    sourceMapFilename: "[file].map"
-  },
-  resolve: {
-    root: [
-      path.resolve('./app')
-    ],
-    extensions: ['', '.js']
-  },
-  devtool: 'sourcemap',
-  debug: true,
-  plugins: [
-    new AppCachePlugin(),
-    new webpack.HotModuleReplacementPlugin(), // Make hot loading work
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.scss$/,
-        loader: 'style!css!sass?includePaths[]=' + path.resolve(__dirname, '../node_modules/compass-mixins/lib')
-      },
-      {
-        test: /\.js$/, // Transform all .js files required somewhere within an entry point
-        loader: 'babel', // 'babel-loader' is also a legal name to reference
-        exclude: [path.join(__dirname, '../node_modules/'),
-                  path.join(__dirname, '../dist/'),
-                  path.join(__dirname, '../server/') ] // except for the node_modules, dist and server folders.
-      },
-      {
-        test: /\.(jpg|png|gif|svg)$/,
-        loader: 'file'
-      }
+    context: paths.context,
+    entry: {
+        js: [
+            './index'
+        ],
+        vendor: [
+            'react', 'react-dom'
+        ]
+    },
+    output: {
+      path: paths.output,
+      filename: '[file].bundle.js',
+      sourceMapFilename: '[file].map'
+    },
+    module: {
+        loaders: [
+          {
+            test: /\.scss$/,
+            loader: 'style!css!sass?includePaths[]=' + path.resolve(__dirname, '../node_modules/compass-mixins/lib')
+          },
+          {
+            test: /\.js$/, // Transform all .js files required somewhere within an entry point
+            loader: 'babel', // 'babel-loader' is also a legal name to reference
+            exclude: [path.join(__dirname, '../node_modules/'),
+                      path.join(__dirname, '../dist/'),
+                      path.join(__dirname, '../server/') ] // except for the node_modules, dist and server folders.
+          },
+          {
+            test: /\.(jpg|png|gif|svg)$/,
+            loader: 'file'
+          }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        modules: [
+            path.resolve('./client'),
+            'node_modules'
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+          name: 'vendor',
+          minChunks: Infinity,
+          filename: 'vendor.bundle.js'
+        }),
     ]
-  },
 }
